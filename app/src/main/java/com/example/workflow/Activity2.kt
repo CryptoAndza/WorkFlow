1c19bfbf7c13df7cb0bce138b91df5a952fd5499
Andza <your@email.com>
2020-08-16 17:07:57 +0100

    1. Hello World! changed to Welcome to WorkFlow
    2. added @string/ for Welcome_to_workflow in activity_main.xml
    because hard coded text should be a string inside strings.xml
    
    3. the added string in strings.xml is <string name="welcome_to_workflow">Welcome to WorkFlow</string>

--- app/src/main/java/com/example/workflow/Activity2.kt
+++ app/src/main/java/com/example/workflow/Activity2.kt
@@ -4,17 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Activity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_2)
 
         // get reference to the second button
-        val button =findViewById<Button>(R.id.button)
+        val button =findViewById<Button>(R.id.button2)
         // attach listener to the button
         button.setOnClickListener {
-            setContentView(R.layout.activity_2)
+            setContentView(R.layout.activity_main)
 
         }
 
