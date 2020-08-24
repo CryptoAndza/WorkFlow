49043f6fe06ef8a0dc5304425e3e8d8f8731fc90
Andza <your@email.com>
2020-08-24 14:09:07 +0100

    1. new Background colour added in all screens including splashscreen
    2. API key correctly added
    
    3. fixed button layout
    
    4. Fragment fixed inside activity_main.xml and activity_maps.xml

--- app/src/main/java/com/example/workflow/MainActivity.kt
+++ app/src/main/java/com/example/workflow/MainActivity.kt
@@ -4,13 +4,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.Maps) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val sydney = LatLng (-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     fun onClickNext( view: View)
@@ -38,6 +58,12 @@ class MainActivity : AppCompatActivity() {
         val newIntent = Intent (this, Activity4::class.java)
         startActivity(newIntent)
     }
+    fun onClickNext4(view: View)
+    {
+        // onClick will trigger new activity to open
+        // Create an Intent to start the second activity
 
-
+        val newIntent = Intent (this, MapsActivity::class.java)
+        startActivity(newIntent)
+    }
 }
